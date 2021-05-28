ec04cf87a5ee4822c8ca3e3cf906769ca7d33aa5
lottejohannessen <77993010+lottejohannessen@users.noreply.github.com>
2021-05-28 11:24:15 -0700

    fixing things

--- src/components/movieCard/movieCard.jsx
+++ src/components/movieCard/movieCard.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
-
-  // constructor() {
-  //   super();
-
-  // }
-
   render() {
     const { movie, onMovieClick } = this.props;
-    return <div onClick={ () => {onMovieClick(movie)} } className="movie-card"> { movie.Title } </div>;
-  }
 
+    return (
+      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+    );
+  }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired
+    }),
+    ImagePath: PropTypes.string.isRequired,
+    Featured: PropTypes.bool.isRequired,
+    Year: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
